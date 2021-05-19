f5e006c2e87b45142d0ba17db6f48b2d9180174e
Nulllink <alexanveld@gmail.com>
2021-05-19 14:43:26 +0300

    3

--- JS/Code.js
+++ JS/Code.js
@@ -3,24 +3,41 @@ function goToPage2(){
 }
 function goToPage()
 {
-    var elem = document.getElementById("inf");
-    if(elem.value != ""){
+    var finish = document.getElementById("inf");
+    var start = document.getElementById("ins");
+    if(start.value != "" && finish.value != "" && start.value != "Откуда" && finish.value != "Куда" && start.value != "Введите значение" && finish.value != "Введите значение"){
         setTimeout(goToPage2,1000)
     }
+    else if(start.value == "" || start.value == "Откуда"){
+        start.style.color = "red";
+        start.value = "Введите значение";
+    }
     else{
-        elem.style.color = "red";
-        elem.value = "Введите значение";
+        finish.style.color = "red";
+        finish.value = "Введите значение";
     }
 }
 function carPush(str){
     var car = document.getElementById('c1');
-    car.style.backgroundColor = "white";
+    car.style.background = "white";
     var car = document.getElementById('c2');
-    car.style.backgroundColor = "white";
+    car.style.background = "white";
     var car = document.getElementById('c3');
-    car.style.backgroundColor = "white";
+    car.style.background = "white";
     var car = document.getElementById('c4');
-    car.style.backgroundColor = "white";
+    car.style.background = "white";
     var elem = document.getElementById(str);
-    elem.style.backgroundColor = "orange";
+    elem.style.background = "linear-gradient(180deg, rgba(186,186,186,1) 0%, rgba(216,216,216,1) 35%, rgba(241,240,240,1) 100%)";
+}
+function delete_text(id){
+    var elem = document.getElementById(id);
+    if(elem.value == "Откуда" || elem.value == "Куда" || elem.value == "Введите значение" || elem.value == "Введите значение"){
+        elem.value = "";
+        elem.style.color = "black";
+    }
+}
+function delete_button(id){
+    var elem = document.getElementById(id);
+    elem.value = "";
+    elem.style.color = "black";
 }
